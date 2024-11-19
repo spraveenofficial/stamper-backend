e04c4c4fe7654b3666ffe20c3dee4d30439d7d7b
Praveen Kumar Singh <spraveen593@gmail.com>
2024-11-20 01:25:31 +0530

    fix: login cookie issue

--- src/app.ts
+++ src/app.ts
@@ -70,6 +70,14 @@ app.use((_req, _res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // Convert error to ApiError, if needed
 app.use(errorConverter);
 
