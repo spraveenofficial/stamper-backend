4c3a1303a405b3f9d20e7b23bb580ff5e547309e
Praveen Kumar Singh <spraveen593@gmail.com>
2025-01-04 23:38:20 +0530

    feat: added get message by chatid

--- src/routes/v1/chat.route.ts
+++ src/routes/v1/chat.route.ts
@@ -5,14 +5,14 @@ import { chatController } from '../../modules/chatv2';
 
 const router: Router = express.Router();
 
-router
-  .route('/create')
-  .post(auth('sendMessage'), chatController.sendMessage);
+router.route('/create').post(auth('sendMessage'), chatController.sendMessage);
 
 router.route('/messages').get(auth(), chatController.getMyMessage);
 
 // Group Chat routes
 
 router.route('/group/create').post(auth(), chatController.createGroupChat);
+
+router.route('/message').get(auth(), chatController.getMessageByChatId);
 
 export default router;
